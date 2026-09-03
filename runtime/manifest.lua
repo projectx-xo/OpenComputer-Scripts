e78c10891aaa54324ce7fafec96e6362337c58c3
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-03 16:26:11 -0400

    chore: release launchpad runtime 2.0.1

--- runtime/manifest.lua
+++ runtime/manifest.lua
@@ -2,11 +2,11 @@ return {
     protocol = 1,
     roles = {
         defense = {
-            version = "2.0.0",
+            version = "2.0.1",
             path = "runtime/launchpad.lua",
         },
         strike = {
-            version = "2.0.0",
+            version = "2.0.1",
             path = "runtime/launchpad.lua",
         },
     },
