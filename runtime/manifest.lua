cd27c6e3a70b865f848b1cdf7bbd5323a822885b
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-03 15:59:40 -0400

    feat: add runtime deployment manifest

---
+++ runtime/manifest.lua
@@ -0,0 +1,13 @@
+return {
+    protocol = 1,
+    roles = {
+        defense = {
+            version = "2.0.0",
+            path = "runtime/launchpad.lua",
+        },
+        strike = {
+            version = "2.0.0",
+            path = "runtime/launchpad.lua",
+        },
+    },
+}
