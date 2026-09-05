b19513a01046293a332bebc74492fb330ca46c5a
projectx-xo <79348979+projectx-xo@users.noreply.github.com>
2026-09-04 22:02:44 -0400

    release: pin STRATCOM 3.0.0 application bundle

---
+++ release.lua
@@ -0,0 +1,18 @@
+return {
+    version = "3.0.0",
+    ref = "d03bad07fd0921910272fa46b2c84d6a94712193",
+    files = {
+        ["central/central.lua"] = {size = 90703, checksum = "aa2247ae"},
+        ["bootstrap/bootstrap.lua"] = {size = 28226, checksum = "949b7352"},
+        ["runtime/manifest.lua"] = {size = 423, checksum = "890768ea"},
+        ["runtime/strike.lua"] = {size = 13612, checksum = "33c3b739"},
+        ["runtime/launchpad.lua"] = {size = 5604, checksum = "e35a811f"},
+        ["runtime/radar.lua"] = {size = 12163, checksum = "92582058"},
+        ["runtime/intel.lua"] = {size = 5927, checksum = "8f99305a"},
+        ["service/stratcom.lua"] = {size = 9599, checksum = "253a3927"},
+        ["service/update.lua"] = {size = 8382, checksum = "8a2d5507"},
+        ["service/rc.lua"] = {size = 349, checksum = "69447d54"},
+        ["service/console.lua"] = {size = 1672, checksum = "f3902bb5"},
+        ["install.lua"] = {size = 5031, checksum = "aa4ecbb1"},
+    },
+}
